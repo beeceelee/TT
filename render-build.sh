ae190b4c4316ca5569ef06724596a934b36a68a2
beeceelee <beeceelee09@gmail.com>
2025-10-03 19:04:30 +0630

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y ffmpeg
-pip install -U yt-dlp
+
+# Update pip
+pip install --upgrade pip
+
+# Install requirements
 pip install -r requirements.txt
