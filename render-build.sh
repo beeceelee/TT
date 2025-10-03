fd7893e38063f8ebc0dfe3f4fdf36c8f6e303919
beeceelee <beeceelee09@gmail.com>
2025-10-03 20:32:52 +0630

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y ffmpeg
-pip install -U yt-dlp
+pip install --upgrade pip
 pip install -r requirements.txt
+pip install https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt_dlp.tar.gz
