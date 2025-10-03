e5826466e670ce83802a898e83c7369f86674c2a
beeceelee <beeceelee09@gmail.com>
2025-10-03 21:12:46 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+Don't give this codes.
+
 import os
 import requests
 from flask import Flask, request
@@ -57,4 +59,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
